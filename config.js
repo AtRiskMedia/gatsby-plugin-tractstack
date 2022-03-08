5f70680a8842c7bf010795c8013fec7086b84844
Adon Irani <a@atriskmedia.com>
2022-03-07 22:10:26 -0500

    It's Alive proper redo

---
+++ config.js
@@ -0,0 +1,6 @@
+export const defaultQueries = {
+  360: '(max-width: 600px)',
+  1080: '(min-width: 601px) and (max-width: 1200px)',
+  1920: '(min-width: 1201px)'
+};
+//# sourceMappingURL=config.js.map
