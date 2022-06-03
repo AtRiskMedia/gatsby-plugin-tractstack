51f1427f7d29ae30ef05a9e8def13d278c7b9181
Adon Irani <a@atriskmedia.com>
2022-06-03 01:18:46 -0400

    implementing recursive build menu function

--- build-menu.js
+++ build-menu.js
@@ -3,24 +3,62 @@ import { Link } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 import { StyledWrapperDiv, InjectCssAnimation } from "./helpers";
 
+const NavLink = ({
+  children,
+  to
+}) => /*#__PURE__*/React.createElement(Link, {
+  to: to,
+  activeClassName: "is-active"
+}, children);
+
+function MenuItems(items, index = 0) {
+  if (typeof items === "undefined") return /*#__PURE__*/React.createElement(React.Fragment, null);
+  let recurse,
+      this_menu_item = /*#__PURE__*/React.createElement(NavLink, {
+    to: `/${items[index]?.field_slug}`
+  }, items[index]?.field_title);
+
+  if (index) {
+    if (items[index]?.field_level === items[index - 1]?.field_level && items[index]?.field_level > items[index + 1]?.field_level || items?.length === index + 1) {
+      return /*#__PURE__*/React.createElement("li", null, this_menu_item);
+    }
+
+    if (items[index]?.field_level === items[index - 1]?.field_level && items[index]?.field_level <= items[index + 1]?.field_level) {
+      recurse = MenuItems(items, index + 1);
+      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("li", null, this_menu_item), recurse);
+    }
+
+    if (items[index]?.field_level > items[index - 1]?.field_level) {
+      // smart split sub-menu from remainder
+      let sub = MenuItems(items, index + 1);
+      if (items?.length >= sub?.props?.children?.length + index + 1) recurse = MenuItems(items, sub?.props?.children?.length + index + 1);
+      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, this_menu_item), sub), recurse);
+    }
+  }
+
+  if (index === 0) {
+    if (items.length) recurse = MenuItems(items, index + 1);
+    return /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, this_menu_item), recurse);
+  }
+}
+
 function BuildMenu(data) {
-  console.log("BuildMenu", data?.payload, data?.state);
+  //console.log("BuildMenu", data?.payload, data?.state, data?.hooks);
   let logo; // svg or image logo?
 
   if (typeof data?.payload?.relationships?.field_svg_logo?.localFile?.publicURL === "string") {
     // svg logo
-    console.log("svg", data?.payload?.relationships?.field_svg_logo);
+    //console.log("svg", data?.payload?.relationships?.field_svg_logo);
     let this_image_id = data?.payload?.relationships?.field_svg_logo?.id;
     let this_image = data?.payload?.relationships?.field_svg_logo?.publicURL;
     logo = /*#__PURE__*/React.createElement("img", {
       src: this_image,
       alt: "Logo"
-    });
-    console.log("svg", logo);
+    }); //console.log("svg", logo);
   } //else
 
 
-  if (typeof data?.payload?.relationships?.field_image_logo?.localFile?.childImageSharp) {
+  if (typeof data?.payload?.relationships?.field_image_logo?.localFile?.childImageSharp[data?.state?.viewport?.viewport?.key] !== "undefined") {
     let this_image_id = data?.payload?.relationships?.field_image_logo?.id;
     let this_image = data?.payload?.relationships?.field_image_logo?.localFile?.childImageSharp[data?.state?.viewport?.viewport?.key]; // image logo
 
@@ -29,15 +67,14 @@ function BuildMenu(data) {
       alt: "Logo",
       image: this_image,
       objectFit: "contain"
-    });
-    console.log("img", logo);
+    }); //console.log("img", logo);
   } else {// no logo
   }
 
-  data?.payload?.relationships?.field_menu_items.map(i => {
-    console.log("MenuItem", i);
+  data?.payload?.relationships?.field_menu_items.map(i => {//console.log("MenuItem", i);
   });
-  return /*#__PURE__*/React.createElement(React.Fragment, null);
+  let menuItems = MenuItems(data?.payload?.relationships?.field_menu_items);
+  return /*#__PURE__*/React.createElement("div", null, menuItems);
 }
 
 export { BuildMenu };
--- src/build-menu.js
+++ src/build-menu.js
@@ -3,8 +3,69 @@ import { Link } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 import { StyledWrapperDiv, InjectCssAnimation } from "./helpers";
 
+const NavLink = ({ children, to }) => (
+  <Link to={to} activeClassName="is-active">
+    {children}
+  </Link>
+);
+
+function MenuItems(items, index = 0) {
+  if (typeof items === "undefined") return <></>;
+  let recurse,
+    this_menu_item = (
+      <NavLink to={`/${items[index]?.field_slug}`}>
+        {items[index]?.field_title}
+      </NavLink>
+    );
+  if (index) {
+    if (
+      (items[index]?.field_level === items[index - 1]?.field_level &&
+        items[index]?.field_level > items[index + 1]?.field_level) ||
+      items?.length === index + 1
+    ) {
+      return <li>{this_menu_item}</li>;
+    }
+    if (
+      items[index]?.field_level === items[index - 1]?.field_level &&
+      items[index]?.field_level <= items[index + 1]?.field_level
+    ) {
+      recurse = MenuItems(items, index + 1);
+      return (
+        <>
+          <li>{this_menu_item}</li>
+          {recurse}
+        </>
+      );
+    }
+    if (items[index]?.field_level > items[index - 1]?.field_level) {
+      // smart split sub-menu from remainder
+      let sub = MenuItems(items, index + 1);
+      if (items?.length >= sub?.props?.children?.length + index + 1)
+        recurse = MenuItems(items, sub?.props?.children?.length + index + 1);
+      return (
+        <>
+          <ul>
+            <li>{this_menu_item}</li>
+            {sub}
+          </ul>
+          {recurse}
+        </>
+      );
+    }
+  }
+  if (index === 0) {
+    if (items.length) recurse = MenuItems(items, index + 1);
+    return (
+      <ul>
+        <li>{this_menu_item}</li>
+        {recurse}
+      </ul>
+    );
+  }
+}
+
 function BuildMenu(data) {
-  console.log("BuildMenu", data?.payload, data?.state);
+  //console.log("BuildMenu", data?.payload, data?.state, data?.hooks);
 
   let logo;
   // svg or image logo?
@@ -13,16 +74,16 @@ function BuildMenu(data) {
       ?.publicURL === "string"
   ) {
     // svg logo
-    console.log("svg", data?.payload?.relationships?.field_svg_logo);
+    //console.log("svg", data?.payload?.relationships?.field_svg_logo);
     let this_image_id = data?.payload?.relationships?.field_svg_logo?.id;
     let this_image = data?.payload?.relationships?.field_svg_logo?.publicURL;
     logo = <img src={this_image} alt="Logo" />;
-    console.log("svg", logo);
+    //console.log("svg", logo);
   }
   //else
   if (
     typeof data?.payload?.relationships?.field_image_logo?.localFile
-      ?.childImageSharp
+      ?.childImageSharp[data?.state?.viewport?.viewport?.key] !== "undefined"
   ) {
     let this_image_id = data?.payload?.relationships?.field_image_logo?.id;
     let this_image =
@@ -37,15 +98,18 @@ function BuildMenu(data) {
         objectFit="contain"
       />
     );
-    console.log("img", logo);
+    //console.log("img", logo);
   } else {
     // no logo
   }
+
   data?.payload?.relationships?.field_menu_items.map((i) => {
-    console.log("MenuItem", i);
+    //console.log("MenuItem", i);
   });
 
-  return <></>;
+  let menuItems = MenuItems(data?.payload?.relationships?.field_menu_items);
+
+  return <div>{menuItems}</div>;
 }
 
 export { BuildMenu };
